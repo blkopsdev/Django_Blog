b1923c7c25b014ede5964f28eaf3d7827fe5259c
doctordev <doctordev0613@gmail.com>
2017-12-16 20:38:14 -0500

    Enabled edition of posts

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
-
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 
 from . models import Post
 
@@ -10,3 +10,13 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields= ['title', 'text']
